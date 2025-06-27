52573ce51568ffe46fec73da83a2a78e99d2629f
MARKAPURAM VENKATA AKHIL <122592444+Akhil244@users.noreply.github.com>
2025-06-27 17:37:25 +0530

    Update tests or logic

--- Week1_design_patterns_and_principles/code/excercise11/CustomerRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-public class CustomerRepositoryImpl implements CustomerRepository {
-   
-    public String findCustomerById(String id) {
-        // Simulate database lookup
-        return "Customer[ID=" + id + ", Name=Venkata Akhil]";
-    }
-}
--- Week1_design_patterns_and_principles/code/excercise4/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void makePayment(double amountInRupees) {
-        System.out.println("Paid ₹" + amountInRupees + " using Stripe.");
-    }
-}
